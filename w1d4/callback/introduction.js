17ee3b194e211f2abf1f815a329e51109ed100f1
RayCJ87 <shihcr.ke@gmail.com>
2018-11-29 14:01:35 +0000

    callback introduction

---
+++ w1d4/callback/introduction.js
@@ -0,0 +1,15 @@
+function findWaldo(arr, found) {
+  arr.forEach(function(element){
+    if (element === "Waldo") {
+      found()
+    }
+  })
+}
+
+
+
+function actionWhenFound() {
+  console.log("Found him!");
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
